1ca0dc0ad4e1e3266bbdc7959b94084e03a7a7b4
shashwat0802 <shashwatgupta4656@gmail.com>
2023-04-13 20:15:49 +0530

    complete roadmap section laptop view

--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -8,7 +8,48 @@ export default function Roadmap() {
         <h1 className="text-white text-center text-2xl md:text-4xl font-extrabold mb-8">
           Roadmap
         </h1>
-        <img src={require('../img/roadmap-content.svg').default} alt="" />
+        <div className="flex justify-around w-[50%]">
+          <img src={require('../img/roadmap-lg.svg').default} alt="" />
+          <div>
+            <div className="roadmap-card px-10 py-2 mb-8">
+              <ul className="text-white font-semibold list-disc">
+                <li className="my-4">Company Establishment</li>
+                <li className="my-4">White Paper Release</li>
+                <li className="my-4">Architecture design</li>
+                <li className="my-4">Seed Private sale</li>
+              </ul>
+            </div>
+            <div className="roadmap-card px-10 py-2 my-8">
+              <ul className="text-white font-semibold list-disc">
+                <li className="my-4">DEX Aggregator Launch</li>
+                <li className="my-4">Strategy Private sale</li>
+                <li className="my-4">DEX Launchpad design</li>
+                <li className="my-4">Public sale</li>
+              </ul>
+            </div>
+            <div className="roadmap-card px-10 py-2 my-8">
+              <ul className="text-white font-semibold list-disc">
+                <li className="my-4">Aggregator & Launchpad integration</li>
+                <li className="my-4">DEX Launchpad Launch</li>
+                <li className="my-4">Crypto Wallet design</li>
+              </ul>
+            </div>
+            <div className="roadmap-card px-10 py-2 my-8">
+              <ul className="text-white font-semibold list-disc">
+                <li className="my-4">DEX & Wallet integration</li>
+                <li className="my-4">Crypto Wallet Launch</li>
+                <li className="my-4">Governance design</li>
+              </ul>
+            </div>
+            <div className="roadmap-card px-10 py-2 my-8">
+              <ul className="text-white font-semibold list-disc">
+                <li className="my-4">Analytics Page</li>
+                <li className="my-4">Governance Protocol Launch</li>
+                <li className="my-4">Platform extension</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
